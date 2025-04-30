f275b8351453696e82963003c6c754ffb61252d9
Copcescki <carlos.copcescki@gmail.com>
2025-04-30 17:24:27 -0300

    Primeiros passos em prolog

---
+++ Exercício 1 - Prolog/ex1.pl
@@ -0,0 +1,30 @@
+% Fatos
+
+animal(urso).
+animal(peixe).
+animal(peixinho).
+animal(lince).
+animal(raposa).
+animal(coelho).
+animal(veado).
+animal(guaxinim).
+planta(alga).
+planta(grama).
+come(urso,peixe).
+come(lince,veado).
+come(urso,raposa).
+come(urso,veado).
+come(peixe,peixinho).
+come(peixinho,alga).
+come(guaxinim,peixe).
+come(raposa,coelho).
+come(coelho,grama).
+come(veado,grama).
+come(urso,guaxinim).
+
+% Respostas:
+
+% planta(x) = false
+% come(raposa,x) = false
+% come(_,_) = true
+% come(x,grama) = false
