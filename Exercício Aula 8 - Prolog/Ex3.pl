1c69857fe75e0bc6707dbde0c4c0707fa0646464
Carlos Copcescki <41136340+carloscopcescki@users.noreply.github.com>
2025-05-13 09:24:01 -0300

    Create Ex3.pl

---
+++ Exercício Aula 8 - Prolog/Ex3.pl
@@ -0,0 +1,21 @@
+% Fatos
+
+recebe(a,a).
+recebe(a,o).
+recebe(b,b).
+recebe(b,o).
+recebe(ab,a).
+recebe(ab,b).
+recebe(ab,ab).
+recebe(ab,o).
+recebe(o,o).
+
+doa(a,a).
+doa(a,ab).
+doa(b,b).
+doa(b,ab).
+doa(ab,ab).
+doa(o,a).
+doa(o,b).
+doa(o,ab).
+doa(o,o).
